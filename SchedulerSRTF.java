6369fe0f2e7620a080a1d794b67b83c20b9405db
Kayden <krazin0202@gmail.com>
2025-03-19 20:22:05 -0600

    SRTF Scheduler completed

--- SchedulerSRTF.java
+++ SchedulerSRTF.java
@@ -1,21 +1,45 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
 
-public class SchedulerSRTF implements Scheduler {
-
+public class SchedulerSRTF extends SchedulerBase implements Scheduler {
+    private Platform platform;
+    private Queue<Process> processesQueue;
     public SchedulerSRTF(Platform platform) {
+        this.platform = platform;
+        this.processesQueue = new PriorityQueue<>((p1, p2) -> Integer.compare((p1.getTotalTime() - p1.getElapsedTotal()), (p2.getTotalTime() - p2.getElapsedTotal())));
+        this.contextSwitches = 0;
     }
     
     @Override
     public int getNumberOfContextSwitches() {
-        return 0;
+        return contextSwitches;
     }
 
     @Override
     public void notifyNewProcess(Process p) {
-        
+        processesQueue.add(p);
     }
 
     @Override
     public Process update(Process cpu) {
+        if (cpu ==null || cpu.isBurstComplete()) {
+            if (cpu != null && cpu.isBurstComplete()) {
+                platform.log("Process " + cpu.getName() + " burst complete");
+            }
+            if (!processesQueue.isEmpty()) {
+                Process nextProcess = processesQueue.poll();
+                platform.log("Scheduled: " + nextProcess.getName());
+                contextSwitches++;
+                return nextProcess;
+            }
+        }
+        else if (!processesQueue.isEmpty() && processesQueue.peek().getTotalTime() - processesQueue.peek().getElapsedTotal() < cpu.getTotalTime() - cpu.getElapsedTotal()) {
+            Process nextProcess = processesQueue.poll();
+            platform.log("Scheduled: " + nextProcess.getName());
+            contextSwitches++;
+            processesQueue.add(cpu);
+            return nextProcess;
+        }
         return cpu;
     }
 
