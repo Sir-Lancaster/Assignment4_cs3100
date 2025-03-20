ed2be7519cd48dfbe5f76b60ebfe080843646a8d
Kayden <krazin0202@gmail.com>
2025-03-19 19:51:15 -0600

    I believe this is the correct design of FCFS

--- SchedulerFCFS.java
+++ SchedulerFCFS.java
@@ -1,21 +1,41 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class SchedulerFCFS implements Scheduler {
+public class SchedulerFCFS extends SchedulerBase implements Scheduler {
+    private Platform platform;
+    private Queue<Process> processesQueue;
+
 
     public SchedulerFCFS(Platform platform) {
+        this.platform = platform;
+        this.processesQueue = new LinkedList<>();
+        this.contextSwitches = 0;
     }
 
     @Override
     public int getNumberOfContextSwitches() {
-        return 0;
+        return contextSwitches;
     }
 
     @Override
     public void notifyNewProcess(Process p) {
-
+        processesQueue.add(p);
     }
 
     @Override
     public Process update(Process cpu) {
+        if (cpu == null || cpu.isBurstComplete()) {
+            if (cpu != null && cpu.isBurstComplete()) {
+                platform.log("Process " + cpu.getName() + " burst complete");
+            }
+            if (!processesQueue.isEmpty()) {
+                Process nextProcess = processesQueue.poll();
+                platform.log("Scheduled: " + nextProcess.getName());
+                contextSwitches++;
+                return nextProcess;
+            }
+        }
+        
         return cpu;
     }
 
